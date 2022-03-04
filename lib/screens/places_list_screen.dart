3abdd7a40ebf5d5689e143e5dcecc74a0e5721ce
Uj5101 <ujvaljkp1234@gmail.com>
2022-03-04 14:38:50 +0530

    Initial commit

---
+++ lib/screens/places_list_screen.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/add_place_screen.dart';
+import '../providers/great_places.dart';
+import '../screens/place_detail_screen.dart';
+
+class PlacesListScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Your Places'),
+          actions: <Widget>[
+            IconButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+              },
+              icon: Icon(Icons.add),
+            ),
+          ],
+        ),
+        body: FutureBuilder(
+          future: Provider.of<GreatPlaces>(context, listen: false)
+              .fetchAndSetPlaces(),
+          builder: (ctx, snapshot) => snapshot.connectionState ==
+                  ConnectionState.waiting
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Consumer<GreatPlaces>(
+                  child: Center(
+                    child: const Text('Got no places yet, start adding some!'),
+                  ),
+                  builder: (ctx, greatPlaces, ch) =>
+                      greatPlaces.items.length <= 0
+                          ? ch
+                          : ListView.builder(
+                              itemBuilder: (ctx, i) => ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage:
+                                      FileImage(greatPlaces.items[i].image),
+                                ),
+                                title: Text(greatPlaces.items[i].title),
+                                subtitle:
+                                    Text(greatPlaces.items[i].location.address),
+                                onTap: () {
+                                  Navigator.of(context).pushNamed(
+                                      PlaceDetailScreen.routeName,
+                                      arguments: greatPlaces.items[i].id);
+                                },
+                              ),
+                              itemCount: greatPlaces.items.length,
+                            ),
+                ),
+        ));
+  }
+}
